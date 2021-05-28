0a5b16299edd80b6fd2dd1685a5f4dd926db558c
Yeastar-zhongwei <zhongwei@yeastar.com>
2021-05-28 08:57:57 +0800

    初始化

---
+++ src/pages/other/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+const Other: React.FC<{}> = () => {
+    return (
+        <div className="routeParent">
+            <div className="route">hello route!</div>
+        </div>
+    );
+};
+
+export default Other
